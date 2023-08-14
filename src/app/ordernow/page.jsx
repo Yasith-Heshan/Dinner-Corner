651ca2372ec972a946f1e4c3428b5e6b12330870
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2023-08-14 07:27:47 +0530

    orderform

--- src/app/ordernow/page.jsx
+++ src/app/ordernow/page.jsx
@@ -129,11 +129,11 @@ const OrderNow = () => {
 
     };
 
-    return (
-      <div className={styles.container}>
-          <h1 className={styles.heading}>අද(13/08/2023) දින මෙම සේවාව ක්‍රියාත්මක නොවේ.</h1>
-      </div>
-    );
+    // return (
+    //   <div className={styles.container}>
+    //       <h1 className={styles.heading}>අද(13/08/2023) දින මෙම සේවාව ක්‍රියාත්මක නොවේ.</h1>
+    //   </div>
+    // );
 
     return (
         <>
