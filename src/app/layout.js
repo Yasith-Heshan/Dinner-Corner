3ea1c3ab79b23e62c39d3430f7b8566ab2c219c3
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2023-08-28 21:41:52 +0530

    meta fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,17 +5,22 @@ import NavBar from "@/components/Navbar/Navbar";
 import {AuthContextProvider} from "@/app/context/AuthContext";
 
 
+
 const inter = Inter({subsets: ['latin']})
 
 
-export const metadata = {
-    title: 'DinnerCorner',
-    description: 'Buy delicious dinner for the lowest price.',
-}
+// export const metadata = {
+//     title: 'DinnerCorner',
+//     description: 'Buy delicious dinner for the lowest price.',
+// }
 
 export default function RootLayout({children}) {
     return (
         <html lang="en">
+        <head>
+            <title>Dinner Corner</title>
+            <meta name='description' content='Buy delicious dinner for the lowest price.' />
+        </head>
         <body className={inter.className}>
         <AuthContextProvider>
             <NavBar/>
