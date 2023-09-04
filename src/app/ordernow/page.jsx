8fa42960f1b232191214ee9c8c812d5f705d4ee5
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2023-09-04 08:05:52 +0530

    cancel orders remobed

--- src/app/ordernow/page.jsx
+++ src/app/ordernow/page.jsx
@@ -216,11 +216,11 @@ const OrderNow = () => {
         setSpecialNotes('');
     }
 
-    return (
-      <div className={styles.container}>
-          <h1 className={styles.heading}>අද(02/09/2023) සහ හෙට(03/09/2023) දින මෙම සේවාව ක්‍රියාත්මක නොවේ.</h1>
-      </div>
-    );
+    // return (
+    //   <div className={styles.container}>
+    //       <h1 className={styles.heading}>අද(02/09/2023) සහ හෙට(03/09/2023) දින මෙම සේවාව ක්‍රියාත්මක නොවේ.</h1>
+    //   </div>
+    // );
 
 
     return (
