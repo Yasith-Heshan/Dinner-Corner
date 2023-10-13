0c5733e36e0dedf2858a757476c12c8e9cfba836
Yasith <yheshan1@gmail.com>
2023-10-13 12:07:54 +0530

    remove fried rice and kottu

--- src/components/Categorise/Categorise.jsx
+++ src/components/Categorise/Categorise.jsx
@@ -19,10 +19,13 @@ const Categorise = ({orders})=>{
     );
 
 
-    const otherListItemIds = [7,8,11,12]
+    const otherListItemIds = []
+    // const otherListItemIds = [7,8,11,12]
     // const otherListItemIds = [11,12]
-    const normalRiceIds = [1,3,5,7,9,11]
-    const fullRiceIds = [2,4,6,8,10,12]
+    const normalRiceIds = [1,3,5,7,9]
+    // const normalRiceIds = [1,3,5,7,9,11]
+    const fullRiceIds = [2,4,6,8,10]
+    // const fullRiceIds = [2,4,6,8,10,12]
 
     const ourList = [];
     const otherList = [];
