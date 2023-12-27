3d01f8bca6945ce60f82f3367f287c5981e7a596
Yasith <yheshan1@gmail.com>
2023-12-28 02:27:47 +0530

    build failing fix

--- src/utils/errorMessages.js
+++ src/utils/errorMessages.js
@@ -3,6 +3,6 @@ import { DEADLINE } from '@/utils/constants';
 export const ORDER_LIMIT_EXCEEDED_ERROR = (date) => {
   return `Sorry, the maximum order capacity for date ${date} has been exceeded`;
 };
-export const LATE_ORDER_ERROR = `Sorry, orders received after ${DEADLINE} will not be accepted due to timely delivery.`;O
+export const LATE_ORDER_ERROR = `Sorry, orders received after ${DEADLINE} will not be accepted due to timely delivery.`;
 
 export const ORDER_FETCHING_ERROR = 'ඇනවුම් ලබා ගැනීම අසාර්තකයි. යම් වරදක් සිදුවී ඇත.'
