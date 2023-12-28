fcc27a54f440d3b2de0f057afae8da4a1020c86b
Yasith <yheshan1@gmail.com>
2023-12-28 09:02:53 +0530

    orders page catagories

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -67,7 +67,7 @@ const AcceptedOrders = () => {
         </div>
       ) : (
         <div>
-          {company_emails.includes(user.email) && (
+          {user && company_emails.includes(user.email) && (
             <Categorise orders={orders} />
           )}
           <div>
@@ -88,7 +88,7 @@ const AcceptedOrders = () => {
                   }
                 })}
           </div>
-          {user && user.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL && (
+          {user && company_emails.includes(user.email) && (
             <Locations orders={orders} />
           )}
         </div>
